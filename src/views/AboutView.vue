670a1042719e269077530da1daf67450ef969f6b
Ahmed-Abdullahi-Hiis <ahmedabdullahihiis@gmail.com>
2025-10-02 14:35:46 +0300

    added services

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an About page</h1>
+    <h1 v-if="isLoginedIn">This is an About page</h1>
+    <h1 v-else>You are not logged in</h1>
     <p>Welcome to CarwashVue 🚗🧽</p>
+    <!-- <h1>{{ isLoginedIn ? 'Welcome back' : 'you are not logged in' }}</h1> -->
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return{
+      isLoginedIn: true,
+    }
+  },
+}
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
